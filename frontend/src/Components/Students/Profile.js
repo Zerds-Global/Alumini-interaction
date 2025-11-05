8a2a31d2b0f8b7eb2690e73661af3023adafe502
ZerDs <zerds.global@gmail.com>
2025-11-06 00:19:30 +0530

    Update Profile.js

--- frontend/src/Components/Students/Profile.js
+++ frontend/src/Components/Students/Profile.js
@@ -77,7 +77,7 @@ function StudentsProfile() {
             try {
                 const token = localStorage.getItem("token");
                 const response = await fetch(
-                    `https://render.com/docs/web-services#port-binding/api/users/${userId}`,
+                    `https://alumini-interaction.onrender.com/api/users/${userId}`,
                     {
                         headers: {
                             "Authorization": `Bearer ${token}`,
